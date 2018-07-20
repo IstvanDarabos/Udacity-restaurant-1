6cd8743466feb486ffb92cb677b5f88063f989ae
István-Darabos <stephendarabos@gmail.com>
2018-07-20 19:21:02 +0200

    sw-test2

--- sw.js
+++ sw.js
@@ -1,6 +1,3 @@
-/** sw.js */
-
-// let staticCacheName = "restaurant-stat-1";
 const staticCacheName = "stat-restaurant-1";
 const urlToCache = [
     '/',
@@ -23,26 +20,6 @@ const urlToCache = [
     '/img/9.jpg',
     '/img/10.jpg',
     // include other new resources for the new version...
-/*    './index.html',
-    './restaurant.html',
-    './css/styles.css',
-    './data/restaurants.json',
-    './js/idb.js',
-    './js/dbhelper.js',
-    './js/main.js',
-    './js/restaurant_info.js',
-    './img/1.jpg',
-    './img/2.jpg',
-    './img/3.jpg',
-    './img/4.jpg',
-    './img/5.jpg',
-    './img/6.jpg',
-    './img/7.jpg',
-    './img/8.jpg',
-    './img/9.jpg',
-    './img/10.jpg',
-    // include other new resources for the new version...
-*/
 ];
 
 self.addEventListener('install', function(event) {
